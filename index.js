603c78cba68fc3da2ee0e9fc4bea8598580d0e5c
Darshan Acharya <acharyadarshan02@gmail.com>
2022-08-17 09:31:58 +0545

    comment to include backend url on frontend request

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-const express = require('express')
-const path = require('path')
-const app = express()
-const PORT = 3001
+// avoid local host connection while deploying at firebase
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = 3001;
 
-app.use("/", express.static(path.join(__dirname, './public/')));
+app.use("/", express.static(path.join(__dirname, "./public/")));
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public/index.html"));
-})
+});
 
 app.listen(PORT, () => {
     console.log(`Running...`);
-})
+});
